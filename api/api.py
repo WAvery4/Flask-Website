284b1bbb566231fc2e13631ea7b77cca6f96eef8
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2021-01-23 17:06:49 -0600

    added basic sliders and backend data generation

--- api/api.py
+++ api/api.py
@@ -1,8 +1,13 @@
-import time
+import random
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/api/data')
+def get_data():
+    choices = [-1, -0.5, 0, 0.5, 1]
+    news_sources = ['xyz.com', 'lion.com', 'usp.com', 'abc.com', 'politexas.com']
+    political_leaning = list(map(lambda x : random.choice(choices), range(5)))
+    source_reputation = list(map(lambda x : random.choice(choices), range(5)))
+    data = [list(a) for a in zip(news_sources, political_leaning, source_reputation)]
+    return {'data': data}
--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
+  const [beliefSlider, setBeliefSlider] = useState(Math.random());
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
+    fetch('/api/data').then(res => res.json()).then(data => {
+      console.log(data)
     });
-  }, []);
+  });
+
+  function sliderInput(e) {
+    e.preventDefault();
+    setBeliefSlider(e.target.value);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-      </header>
+      <div class="beliefslidecontainer">
+        <input type="range" min="-5" max="5" className="beliefslider" step="0.5" value={beliefSlider} onInput={sliderInput} />
+      </div>
     </div>
   );
 }
