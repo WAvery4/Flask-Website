b77db9750d377cafe79d6f5417924671df509d68
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2021-01-24 00:21:31 -0600

    possible fix

--- api.py
+++ api.py
@@ -13,4 +13,6 @@ def manipulate_data():
     data = [["abc.com", 1, 0], ["lion.com", 0.5, 0.5], ["xyz.com", -0.5, 1], ["politexas.com", -1, 0], ["usp.com", 0, 0]]
     return {'data': data}
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
