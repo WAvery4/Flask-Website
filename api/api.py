9e4cd93cf0dc82139cd34f6390c772a81101d7f0
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2021-01-23 23:23:59 -0600

    added heroku deployment files

--- api/api.py
+++ api/api.py
@@ -1,11 +1,14 @@
 import random
 from flask import Flask
-from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 # simulate communication with a database
-@app.route('/api/data', methods = ['GET', 'POST'])
+@app.route('/api/data')
 def manipulate_data():
     data = [["abc.com", 1, 0], ["lion.com", 0.5, 0.5], ["xyz.com", -0.5, 1], ["politexas.com", -1, 0], ["usp.com", 0, 0]]
     return {'data': data}
